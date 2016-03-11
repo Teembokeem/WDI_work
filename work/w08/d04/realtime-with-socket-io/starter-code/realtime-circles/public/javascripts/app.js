56eb85aee3e7d654a7a2b2ec90cc615f21f3861d
myunghoon kim <myungtkim@gmail.com>
2016-03-11 09:13:34 -0800

    grabbing work from w08-d03

--- work/w08/d04/realtime-with-socket-io/starter-code/realtime-circles/public/javascripts/app.js
+++ work/w08/d04/realtime-with-socket-io/starter-code/realtime-circles/public/javascripts/app.js
@@ -1,32 +1,72 @@
-document.addEventListener("DOMContentLoaded", function() { 
-  
+document.addEventListener("DOMContentLoaded", function() {
+
+  var socket = io();
+    // listen to the server for the `add-circle` event
+    socket.on('add-circle', function (data) {
+      addCircle(data);
+    });
+
+    socket.on('clear', function (data) {
+      deleteCircle(data)
+    });
+
+    socket.on('update-player-list', function (data) {
+      var playerList = '<li>' + data.join('</li><li>') + '</li>';
+      players.innerHTML = playerList;
+    });
+
   var circles = document.getElementById('circles');
+  var players = document.getElementById('players');
   var initials = '';
 
   circles.addEventListener('click', function(evt) {
-    addCircle(evt.clientX, evt.clientY, randomBetween(10,125), getRandomRGBA());
+    socket.emit('add-circle', {
+        initials: initials,
+        x: evt.clientX,
+        y: evt.clientY,
+        dia: randomBetween(10,100),
+        rgba: getRandomRGBA()
+    });
   });
 
   document.getElementsByTagName('button')[0].addEventListener('click', function() {
-    circles.innerHTML = '';
+    socket.emit('clear', {
+      innerhtml: ""
+    })
   });
+
+
+while (initials.length < 2 || initials.length > 3) {
+    initials = prompt("Please enter your initials").toUpperCase();
+    // new code below
+    if (initials.length > 1 && initials.length < 4) {
+        socket.emit('register-player', {initials: initials});
+    }
+}
+
+
+  function deleteCircle(data) {
+    circles.innerHTML = data.innerhtml
+  }
 
   while (initials.length < 2 || initials.length > 3) {
     initials = prompt("Please enter your initials").toUpperCase();
   }
 
-  function addCircle(x, y, dia, rgba) {
+  function addCircle(data) {
     var el = document.createElement('div');
-    el.style.left = x - Math.floor(dia / 2 + 0.5) + 'px';
-    el.style.top = y - Math.floor(dia / 2 + 0.5) + 'px';
-    el.style.width = el.style.height = dia + 'px';
-    el.style.backgroundColor = rgba;
-    el.style.fontSize = Math.floor(dia / 3) + 'px';
+    el.style.left = data.x - Math.floor(data.dia / 2 + 0.5) + 'px';
+    el.style.top = data.y - Math.floor(data.dia / 2 + 0.5) + 'px';
+    el.style.width = el.style.height = data.dia + 'px';
+    el.style.backgroundColor = data.rgba;
+    el.style.fontSize = Math.floor(data.dia / 3) + 'px';
     el.style.color = 'white';
     el.style.textAlign = 'center';
-    el.style.lineHeight = dia + 'px';
-    el.innerHTML = initials;
+    el.style.lineHeight = data.dia + 'px';
+    el.style.border = '2px solid ' + data.rgba;
+    el.innerHTML = data.initials;
     circles.appendChild(el);
+    document.body.style.backgroundColor = data.rgba;
   }
 
   function randomBetween(min, max) {
@@ -35,7 +75,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
   function getRandomRGBA() {
     return ['rgba(', randomBetween(0, 255), ',', randomBetween(0, 255), ',',
-      randomBetween(0, 255), ',', randomBetween(2, 10) / 10, ')'].join(''); 
+      randomBetween(0, 255), ',', randomBetween(2, 10) / 10, ')'].join('');
   }
 
 });

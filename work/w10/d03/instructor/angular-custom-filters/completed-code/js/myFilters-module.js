c6b5a16810b251f2c33fd3d8c6e0737fb407573b
jim-clark <email@jim-clark.com>
2016-03-25 11:03:38 -0700

    Add angular-custom-filters completed code

---
+++ work/w10/d03/instructor/angular-custom-filters/completed-code/js/myFilters-module.js
@@ -0,0 +1,31 @@
+(function() {
+  'use strict';
+
+  angular.module('myFilters', [])
+
+    .filter('firstName', function() {
+
+      return function(fullName) {
+        var firstName;
+
+        firstName = fullName.slice(0, fullName.indexOf(' '));
+
+        return firstName;
+      };
+
+    })
+
+    .filter('ellipsis', function() {
+
+      return function(text, trimSize) {
+        var trimmed;
+        var ellipsisCharCode = '\u2026';
+
+        trimmed = text.slice(0, trimSize) + ellipsisCharCode;
+
+        return trimmed;
+      };
+
+    });
+
+})();

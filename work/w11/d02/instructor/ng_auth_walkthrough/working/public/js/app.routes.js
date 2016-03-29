9db7c49b200731fcab792748957ce5d1f48c28e1
h4w5 <pj@ga.co>
2016-03-29 11:49:34 -0700

    add routes

---
+++ work/w11/d02/instructor/ng_auth_walkthrough/working/public/js/app.routes.js
@@ -0,0 +1,28 @@
+(function() {
+  "use strict";
+
+  angular
+    .module("app")
+    .config(appRoutes);
+
+  appRoutes.$inject = ["$urlRouterProvider", "$stateProvider"];
+
+  function appRoutes($urlRouterProvider, $stateProvider) {
+    $stateProvider
+      .state("welcome", {
+        url:         "/",
+        templateUrl: "/js/welcome.html"
+      })
+      .state("signin", {
+        url:         "/signin",
+        templateUrl: "/js/signin.html"
+      })
+      .state("profile", {
+        url:         "/profile",
+        templateUrl: "/js/profile.html"
+      });
+
+    $urlRouterProvider.otherwise("/");
+  }
+
+})();
